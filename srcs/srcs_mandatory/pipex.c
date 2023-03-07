0b6d043e98936c09302020c61baf6b6f4f3311fe
Vinni-Cedraz <Vinni.vcr@gmail.com>
2023-03-06 21:46:48 -0300

    ❯ ./pipex input_file cat "grep ice" file2
    file1: input_file
    input_fd: 5
    file2: file2
    output_fd: 6
    /usr/bin/grep: file2: input file is also the output

--- srcs/srcs_mandatory/pipex.c
+++ srcs/srcs_mandatory/pipex.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 18:33:38 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/03/06 20:30:45 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/03/06 21:44:15 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,21 @@ int	main(int argc, char **argv)
 	d = (t_data *)malloc(sizeof(t_data));
 	init_data(d, argv, argc);
 	create_pipe(d);
+	open_input_file(d);
+	open_output_file(d);
 	create_child_process(d);
 	if (is_child_process(d->fork.id))
 	{
-		open_input_file(d);
 		redirect_stdin_to_file_one(d);
 		redirect_stdout_to_pipe(d);
 		execute_first_command(d);
-		close(d->pipe.input_fd);
 	}
 	else
 	{
 		wait(0);
-		open_output_file(d);
 		redirect_stdin_to_pipe(d);
 		redirect_stdout_to_file_two(d);
 		execute_second_command(d);
-		close(d->pipe.output_fd);
-		destroy_args_args(d);
-		free(d);
+		destroy_data(d);
 	}
 }
