3cdd6e01da9e43e7d54135b4a84a5ef15a480c81
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-03-09 08:35:12 -0300

    MVP --no-mem-leaks

--- srcs/srcs_mandatory/continue_parent_process.c
+++ srcs/srcs_mandatory/continue_parent_process.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 14:45:49 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/03/09 08:30:51 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/03/09 08:34:30 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,7 @@ static inline void	destroy_data(t_data *d)
 	ft_free_arr(d->execve.str_arr2, (void **)d->execve.str_arr2);
 	ft_free_t_split(d->split1);
 	ft_free_t_split(d->split2);
+	free(d->execve.full_cmd1);
+	free(d->execve.full_cmd2);
 	free(d);
 }
