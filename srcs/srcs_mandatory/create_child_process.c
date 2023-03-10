bec7dfa7cbed5ebe3f1f63a4acb7a04aade3fd31
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-03-10 17:08:43 -0300

    timeout resolvido

--- srcs/srcs_mandatory/create_child_process.c
+++ srcs/srcs_mandatory/create_child_process.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 13:44:02 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/03/09 16:52:01 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/03/10 16:55:25 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	wait_and_create_second_child(t_data *d)
 {
 	int	id;
 
-	wait(0);
+	// wait(0);
 	id = fork();
 	if (-1 == id)
 		handle_error(d, "second_fork", &free_error4);
--- srcs/srcs_mandatory/open_input_output_files.c
+++ srcs/srcs_mandatory/open_input_output_files.c
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 15:25:13 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/03/10 12:47:06 by vcedraz-         ###   ########.fr       */
+/*   Updjted: 2023/03/10 16:58:53 by vcedraz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ void	open_input_output_files(t_data *d)
 	t_fds	*fds;
 
 	fds = &d->file_descriptors;
-	if (-1 == access(d->args.file1, R_OK))
-		handle_error(d, "can't access input file", &free_error2);
+	if (-1 == access(d->args.file1, R_OK | F_OK))
+		handle_error(d, "input file doesn't exist or is not readable", NULL);
 	fds->input_fd = open(d->args.file1, O_RDONLY);
-	if (fds->input_fd == -1)
+	if (-1 == fds->input_fd)
 		handle_error(d, "open input file", &free_error2);
 	fds->output_fd = open(d->args.file2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fds->output_fd == -1)
+	if (-1 == fds->output_fd)
 		handle_error(d, "open output file", &free_error3);
 }
